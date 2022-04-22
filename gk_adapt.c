a5472d39858b1bdeff53a4fc040b2e13f01be531
Kirill Terekhov <kirill-terekhov@users.noreply.github.com>
2022-04-22 17:18:42 +0300

    Update gk_adapt.c
    make linux compiler happy

--- gk_adapt.c
+++ gk_adapt.c
@@ -2,11 +2,12 @@
 \file  win32/adapt.c
 \brief Implementation of Win32 adaptation of libc functions
 */
-#if defined(_WIN32)
 #include "gk_adapt.h"
-
+#if defined(_WIN32)
 pid_t getpid(void)
 {
   return GetCurrentProcessId();
 }
 #endif
+
+typedef int make_iso_compilers_happy;
